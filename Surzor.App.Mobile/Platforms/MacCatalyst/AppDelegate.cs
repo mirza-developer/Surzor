89fb2600da520c419f4b62301e7547ff5214d652
artav <artav.work@outlook.com>
2022-11-08 22:36:07 +0330

    Add maui blazor to solution

---
+++ Surzor.App.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Surzor.App.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
